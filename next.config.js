02677c845a8c3854c0b157d3f6cf9f72ca85c9ec
warning11223 <warning11223@gmail.com>
2023-09-23 23:02:46 +0300

    refactor: add domains: ["image.tmdb.org"],

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.tmdb.org",
-      },
-      { hostname: "dummyimage.com" },
-    ],
-    domains: ["image.tmdb.org"],
+    domains: ["image.tmdb.org", "dummyimage.com"],
   },
 };
 
